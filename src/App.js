d8521ed129aeabf701fc005c731d26ffb3d7e20a
Joonas Rantanen <joonas.rantanen91@gmail.com>
2018-08-14 21:33:30 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './index.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props) {
+      this.state = {
+        lampotila: null
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-	tervetuloa
+        <h2>Lämpötila</h2>
+        
       </div>
     );
   }
